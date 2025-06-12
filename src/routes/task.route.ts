5fb02b6035d0c111aabdcd3f3dbd639ec0f8879c
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-06-11 20:35:07 -0400

    (config): add custom middleware to the routes

--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+
+import { validateTask } from '../middlewares/validateTask';
+import { handleValidation } from '../middlewares/handleValidation';
 import { getTasks, addTask, editTask, removeTask } from '../controllers/task.controller';
 
 const router = express.Router();
 
 router.get('/:userId', getTasks);
-router.post('/', addTask);
+router.post('/', validateTask, handleValidation, addTask);
 router.put('/:id', editTask);
 router.delete('/:id', removeTask);
 
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { getUser, registerUser } from '../controllers/user.controller';
+import { validateEmail } from '../middlewares/validateEmail';
+import { handleValidation } from '../middlewares/handleValidation';
 
 const router = express.Router();
 
 router.get('/:email', getUser);
-router.post('/', registerUser);
+router.post('/', validateEmail, handleValidation, registerUser);
 
 export default router;

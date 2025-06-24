84cf4a321e61e6195bcca7fdb2912c02e4d67c4e
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-06-24 11:32:22 -0400

    feat: changes in validation

--- functions/src/middlewares/validateTask.ts
+++ functions/src/middlewares/validateTask.ts
@@ -1,16 +1,27 @@
 import { body } from "express-validator";
 
+const MAX_TITLE_LENGTH = 50;
+const MAX_DESCRIPTION_LENGTH = 200;
+
 export const validateTask = [
   body("title")
     .notEmpty()
     .withMessage("El título es obligatorio")
     .isString()
-    .withMessage("El título debe ser un texto"),
+    .withMessage("El título debe ser un texto")
+    .isLength({ max: MAX_TITLE_LENGTH })
+    .withMessage(
+      `El título no puede exceder los ${MAX_TITLE_LENGTH} caracteres`
+    ),
 
   body("description")
     .optional()
     .isString()
-    .withMessage("La descripción debe ser un texto"),
+    .withMessage("La descripción debe ser un texto")
+    .isLength({ max: MAX_DESCRIPTION_LENGTH })
+    .withMessage(
+      `La descripción no puede exceder los ${MAX_DESCRIPTION_LENGTH} caracteres`
+    ),
 
   body("completed")
     .optional()

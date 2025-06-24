9785f2b2ca229e2bd05470f77e7e8ad938a72f11
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-06-24 09:16:40 -0400

    change project to new one

--- functions/test.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config();
-console.log("PROJECT_ID:", process.env.FIREBASE_PROJECT_ID);
-console.log("FIREBASE_CLIENT_EMAIL:", process.env.FIREBASE_CLIENT_EMAIL);
-console.log("FIREBASE_PRIVATE_KEY:", process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'));
